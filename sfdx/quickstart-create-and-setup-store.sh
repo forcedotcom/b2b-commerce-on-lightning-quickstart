68fed61a5280a267a35ccd03e1cf75558fdc552b
Oleksii Golub <oleksii.v.golub@gmail.com>
2024-01-03 02:11:55 +0200

    update readme and update bash scripts to use sf and remove deprecation

--- sfdx/quickstart-create-and-setup-store.sh
+++ sfdx/quickstart-create-and-setup-store.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # Use this command to create a new store.
 # The name of the store can be passed as a parameter.
+export SF_NPM_REGISTRY="http://platform-cli-registry.eng.sfdc.net:4880/"
+export SF_S3_HOST="http://platform-cli-s3.eng.sfdc.net:9000/sfdx/media/salesforce-cli"
 
-export SFDX_NPM_REGISTRY="http://platform-cli-registry.eng.sfdc.net:4880/"
-export SFDX_S3_HOST="http://platform-cli-s3.eng.sfdc.net:9000/sfdx/media/salesforce-cli"
-
-#templateName="b2c-lite-storefront"
-templateName="B2B Commerce"
+templateName="B2B Commerce (LWR)"
 
 function echo_attention() {
   local green='\033[0;32m'
@@ -28,7 +26,9 @@ then
 else
     storename=$1
 fi
-sfdx force:community:create --name "$storename" --templatename "B2B Commerce" --urlpathprefix "$storename" --description "Store $storename created by Quick Start script."
+
+sf community create --name "$storename" --template-name "$templateName" --url-path-prefix "$storename" --description "Store $storename created by Quick Start script."
+
 echo ""
 
 storeId=""
@@ -36,7 +36,7 @@ storeId=""
 while [ -z "${storeId}" ];
 do
     echo_attention "Store not yet created, waiting 10 seconds..."
-    storeId=$(sfdx force:data:soql:query -q "SELECT Id FROM WebStore WHERE Name='${storename}' LIMIT 1" -r csv |tail -n +2)
+    storeId=$(sf data query -q "SELECT Id FROM WebStore WHERE Name='${storename}' LIMIT 1" -r csv |tail -n +2)
     sleep 10
 done
 
@@ -47,7 +47,7 @@ echo ""
 
 echo_attention "Pushing store sources..."
 set -x
-sfdx force:source:push -f
+sf project deploy start -c
 set +x
 
 echo ""
